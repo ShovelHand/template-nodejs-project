19f5550345aadd1c6a704cca30eeb1bf2cf887d6
Alex Carmichael <carmichael.alexander@gmail.com>
2022-05-15 22:16:36 -0700

    Now listing entities in view.

--- web/components/jellicleCatsList.tsx
+++ web/components/jellicleCatsList.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 
 export const SampleEntityList: React.FC<any> = () => {
 	const [entities, setEntities] = useState<Object>({});
+	const [entityList, setEntityList] = useState<any>();
 	const getEntities = () => {
 		const xhr = new XMLHttpRequest();
 		xhr.open('get', '/cats/list', true);
@@ -24,21 +25,21 @@ export const SampleEntityList: React.FC<any> = () => {
 		xhr.send();
 	};
 
-	let entityList = "";
 	getEntities();
+
+	/*note that the xhr call above could return the data as an object, but handling Objects with useEffect reults in an infinite loop. */
 	useEffect(() => {
-		console.log((entities));
-		if (entities[0]) {
+		if (entities.length) {
 			const entitiesAsObject = JSON.parse(entities);
-			console.log(entitiesAsObject);
-			console.log(entitiesAsObject[0]["name"]);
-		//	const names = entities.map((cat) =>
-			//	JSON.stringify(cat));
+			const names = entitiesAsObject.map((cat) =>
+				<li>{ cat.name }</li>
+			);
+			setEntityList(names);
 		}
 	}, [entities]);
 
 	return (
-		entityList
+		<ul>{entityList}</ul>
 	);
 };
 
